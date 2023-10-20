0f0df16dd27c3e5319ce0947d4ab27e256443d1b
Blanca Penabad <a22blancapv@iessanclemente.net>
2023-10-20 16:55:47 +0200

    Tarea1

--- www/ud02/01introduccion/tarea1.php
+++ www/ud02/01introduccion/tarea1.php
@@ -8,3 +8,16 @@ echo 'Estoy bien, gracias.';
 ??>
 ```
 **/
+?>
+
+<?php
+/*SOLUCIÓN:
+La etiqueta de cierre es incorrecta. Sólo se cierra con una interrogación. Para una mejor
+legibilidad he añadido un salto de línea.
+Resultado:
+*/
+
+echo '¿Cómo estás?<br/>';
+echo 'Estoy bien, gracias.';
+?>
+
