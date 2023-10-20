b43b307ecbcd06440322a0963425c4feb0d506b9
Blanca Penabad <a22blancapv@iessanclemente.net>
2023-10-20 17:09:53 +0200

    Tarea6

--- www/ud02/01introduccion/tarea6.php
+++ www/ud02/01introduccion/tarea6.php
@@ -2,6 +2,12 @@
 
 //Usando la instrucción ```echo``` crea un programa en PHP que muestre el mensaje: ```Hola, Mundo!```  Optativo. Muéstralo en cursiva.
 
+//SOLUCIÓN:
+echo "Hola, Mundo!<br/>"."En cursiva: <i>Hola, Mundo!</i>";
+
 //Crea un programa en PHP que guarde en una variable tu nombre y lo muestre en negrita formando el siguiente mensaje: Bienvenido tunombre
 
+//SOLUCIÓN:
+$miNombre="Blanca Penabad Villar";
+echo "<br/>Bienvenida ".$miNombre;
 ?>
