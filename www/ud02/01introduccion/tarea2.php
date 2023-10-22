523284838671800f67711625021f79fdb74c08a9
Blanca Penabad <a22blancapv@iessanclemente.net>
2023-10-22 23:04:00 +0200

    Tarea2

--- www/ud02/01introduccion/tarea2.php
+++ www/ud02/01introduccion/tarea2.php
@@ -1,27 +1,27 @@
 <?php
 
 /**
- * Indica cuál de los siguientes son nombres de variables válidas e inválidos, indica por qué (en comentarios) y corrige los fallos:
-- valor 
-- $_N
-- $valor_actual 
-- $n
-- $#datos 
-- $valorInicial0
-- $proba,valor 
-- $2saldo
-- $n
-- $meuProblema
-- $meu Problema
-- $echo
-- $m&m
-- $registro
-- $ABC
-- $85 Nome
-- $AAAAAAAAA
-- $nome_apelidos
-- $saldoActual
-- $92
-- $*143idade
+  Indica cuál de los siguientes son nombres de variables válidas e inválidos, indica por qué (en comentarios) y corrige los fallos:
+- valor //Incorrecto. Una variable se indica con el símbolo del dólar. Debería ser $valor
+- $_N //Correcto. El nombre de las variables pueden comenzar por una letra o símbolo _
+- $valor_actual //Correcto
+- $n //Correcto
+- $#datos //Incorrecto. Las variables SÓLO puede empezar por una letra o por el símbolo _ además no pueden conterner caracteres especiales.
+- $valorInicial0 //Correcto
+- $proba,valor  //Incorrecto. La coma es un caracter especial el cual NO es válido.
+- $2saldo //Incorrecto. El nombre de la variable NO puede empezar por números. Sólo letras y símbolo _
+- $n //Correcto
+- $meuProblema //Correcto
+- $meu Problema //Incorrecto. Los nombres de las variables NO pueden contener espacios.
+- $echo //Correcto
+- $m&m //Incorrecto. Los nombres de las variables NO pueden contener caracteres especiales como "&"
+- $registro //Correcto
+- $ABC //Correcto
+- $85 Nome //Incorrecto. Las variables NO pueden empezar por números ni tampoco contener espacios.
+- $AAAAAAAAA //Correcto
+- $nome_apelidos //Correcto
+- $saldoActual //Correcto
+- $92 //Incorrecto. Las variables NO pueden empezar por números, SI pueden contenerlos pero no comenzar por ellos.
+- $*143idade //Incorrecto. 
  */
 ?>
