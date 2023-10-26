8c53d42ab433a19cc904b847f5e07c22bbcaf976
Blanca Penabad <a22blancapv@iessanclemente.net>
2023-10-26 11:46:30 +0200

    Tarea3

--- www/ud02/02arrays/tarea3.php
+++ www/ud02/02arrays/tarea3.php
@@ -38,10 +38,50 @@ foreach($datos as $d){
     echo $d."  ";
 }
 
+echo" "."</br>"." "."</br>";
+echo "Elimino la última posición. Resultado: </br>";
+$personajes = array_pop($datos);
+foreach($datos as $d){
+    echo $d."  ";
+}
+
+echo" "."</br>"." "."</br>";
+echo "Posición donde se encuentra \"Superman\": </br>";
+$key = array_search("Superman",$datos);
+    echo $key;
+
+  
+echo" "."</br>"." "."</br>";
+echo "Añado nuevos personajes al final del array. Resultado: </br>";
+array_push($datos,"Carl","Lenny","Burns","Lisa");
+foreach($datos as $d){
+    echo $d."  ";
+}
+
+echo" "."</br>"." "."</br>";
+echo "Ordeno alfabéticamente el array. Resultado: </br>";
+asort($datos);
+foreach($datos as $d){
+    echo $d."  ";
+}
+
+echo" "."</br>"." "."</br>";
+echo "Añado nuevos datos al comienzo del array. Resultado: </br>";
+array_unshift($datos,"Apple","Melon","Watermelon");
+foreach($datos as $d){
+    echo $d."  ";
+}
+
 
 
 /*3. (Optativo) Cree una copia de la matriz con el nombre `copia` con elementos del 3 al 5.
     - Agregue el elemento 'Pera' al final de la matriz. */ 
 
-
+echo" "."</br>"." "."</br>";
+echo "Nuevo array \"Copia\" con los elementos del 3 al 5. Añado \"Pera\" al final del nuevo array. Resultado: </br>";
+$copia = array_slice($datos, 3, 5); 
+array_push($copia,"Pera");
+foreach($copia as $c){
+    echo $c."  ";
+}
 ?>
