f00e15bf82f5f9e92b3823d93c6a7c3ac254e8f5
Blanca Penabad <a22blancapv@iessanclemente.net>
2023-10-26 23:17:48 +0200

    Formularios

--- www/ud02/04formularios/tarea1.php
+++ www/ud02/04formularios/tarea1.php
@@ -6,6 +6,13 @@
     <body>
         <div>
             <!-- Aquí va el formulario-->
+            <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>"> 
+            Nombre: <input type="text" name="nombre">
+            <br><br>
+            Apellidos: <input type="text" name="apellidos">
+            <br><br>
+            <input type="submit" name="submit" value="Submit">  
+            </form>
         </div>
 
             <div>
@@ -24,6 +31,18 @@
                     */
                     
                     //Aquí el código php que muestra la información solicitada.
+                    $name = $_POST["nombre"];
+                    $surname = $_POST["apellidos"];
+                    echo "Nombre: ".$name."</br>";
+                    echo "Apellidos: ".$surname."</br>";
+                    echo "Nombre y apellidos: ".$_POST["nombre"]." ".$_POST["apellidos"]."</br>";
+                    echo "Su nombre tiene ".strlen($name)." caracteres</br>";
+                    echo "Los 3 primeros caracteres de su nombre son: ".substr($name,0,3)."</br>";
+                    echo "La letra A fue encontrada en sus apellidos en la posición: ".strpos($surname,"a",0)."</br>";
+                    echo "Su nombre en mayúsculas es: ".strtoupper($name)."</br>";
+                    echo "Sus apellidos en minúsculas son: ".strtolower($surname)."</br>";
+                    echo "Su nombre y apellido en mayúsculas:".strtoupper($name)." ".strtoupper($surname)."</br>";
+                    echo "Su nombre escrito al revés es: ".strrev($name)."</br>";
                 ?>
         </div>
     </body>
