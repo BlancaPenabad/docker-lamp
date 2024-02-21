e87ecfd864e5583dafdbf571cf4398d91a4fc113
Blanca Penabad <a22blancapv@iessanclemente.net>
2024-02-21 17:26:54 +0100

    Retoques 04Anónimas

--- www/ud05/04Anonimas/ClaseAnonima.php
+++ www/ud05/04Anonimas/ClaseAnonima.php
@@ -1,6 +1,7 @@
 <?php
 
 $claseAnonima = new Class($base = 0, $altura =0){
+    //$claseAnonima = new Class(){
 
     private $base;
     private $altura;
@@ -14,11 +15,21 @@ $claseAnonima = new Class($base = 0, $altura =0){
         $area = ($this->base*$this->altura)/2;
         return $area;
     }
-
+    /*
+    public function area($base, $altura){
+        $area = ($base*$altura)/2;
+        return $area;
+    }
+    */
     public function perimetro(){
         $perimetro = (2*$this->base+2*$this->altura);
         return $perimetro;
     }
+    /*
+    public function perimetro($base, $altura){
+        $perimetro = (2*$base+2*$altura);
+        return $perimetro;
+    }
+    */
 }
-
 ?>
