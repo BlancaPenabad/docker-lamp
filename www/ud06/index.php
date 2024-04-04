559b6e777ac9533bac403a223a41918a517e8777
Blanca Penabad <a22blancapv@iessanclemente.net>
2024-04-04 22:49:02 +0200

    Function getHoteles()

--- www/ud06/index.php
+++ www/ud06/index.php
@@ -3,6 +3,7 @@
 // require 'flight/Flight.php';
 require 'flight/autoload.php';
 require 'tablaClientes.php';
+require 'tablaHoteles.php';
 
 Flight::register('db', 'PDO', array('mysql:host=db;dbname=pruebas','root','test'));
 
@@ -12,6 +13,10 @@ Flight::route('POST /clientes', 'addCliente'); //Para a√±adir datos usamos el m√
 Flight::route('DELETE /clientes', 'deleteCliente');
 Flight::route('PUT /clientes', 'modificaCliente');
 Flight::route('GET /cliente', 'getCliente'); 
+
+
+//Tabla hoteles
+Flight::route('GET /hoteles', 'getHoteles');
 
 
 Flight::start();
---
+++ www/ud06/tablaHoteles.php
@@ -0,0 +1,11 @@
+<?php
+
+function getHoteles(){
+    $stmt = Flight::db()->prepare("SELECT * FROM hoteles"); //Preparamos la sentencia sql de SELECT a través del Flight.
+    $stmt->execute(); //Ejecutamos la sentencia anterior.
+    $datos = $stmt->fetchAll(); //Recuperamos los datos en un array 'datos'.
+    Flight::json($datos); //Imprimimos los datos en formato json.
+}
+
+
+?>
