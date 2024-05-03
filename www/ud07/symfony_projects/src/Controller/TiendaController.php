63b9e4634d4135717e1ecfa2e1d21ef1c89c0aef
Blanca Penabad <a22blancapv@iessanclemente.net>
2024-05-03 13:14:24 +0200

    Controller y homepage de la panadería

--- www/ud07/symfony_projects/src/Controller/TiendaController.php
+++ www/ud07/symfony_projects/src/Controller/TiendaController.php
@@ -6,20 +6,46 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TiendaController extends AbstractController{
 
-    #[Route('/')]
-    public function homepage(){
+    private $productos;
+    private $nombre;
 
-        $productos = [
-            'Molete pan',
-            'Barra trigo país',
-            'Barra integral',
-            'Chapata',
-            'Molete centeno'
+    public function __construct() {
+        
+        $this->productos = [
+            ['descripcion' =>'Baguette', 
+            'foto' => 'images/baguette.jpeg',
+            'precio' => '2,50€'],
+
+            ['descripcion' => 'Boule', 
+            'foto' => 'images/boule.jpeg',
+            'precio' => '1,50€'],
+
+            ['descripcion' => 'Brioche', 
+            'foto' => 'images/brioche.jpeg',
+            'precio' => '1,50€'],
+
+            ['descripcion' => 'Croissant mantequilla',
+             'foto' => 'images/croissant.jpg',
+             'precio' => '1,20€'],
+
+            ['descripcion' => 'Pain au chocolat', 
+            'foto' => 'images/painauchocolat.jpeg',
+            'precio' => '1,50€']
+
+            
+            
         ];
 
+        $this->nombre = 'Boulangerie LeBlanc';
+    }
+
+        
+
+        #[Route('/')]
+    public function homepage(){
         return $this->render('tienda/homepage.html.twig',[
-            'titulo' => 'Tienda Blanca',
-            'productos' => $productos,
+            'titulo' => $this->nombre,
+            'productos' => $this->productos
 
         ]);
     }
