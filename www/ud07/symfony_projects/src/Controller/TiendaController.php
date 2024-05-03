b302f3c661ed850f2d4cbd9c32eb184614918c7a
Blanca Penabad <a22blancapv@iessanclemente.net>
2024-05-03 13:47:02 +0200

    Plantilla boulangerie

--- www/ud07/symfony_projects/src/Controller/TiendaController.php
+++ www/ud07/symfony_projects/src/Controller/TiendaController.php
@@ -20,16 +20,33 @@ class TiendaController extends AbstractController{
             'foto' => 'images/boule.jpeg',
             'precio' => '1,50€'],
 
+            ['descripcion' => 'Bretzel', 
+            'foto' => 'images/bretzel.jpeg',
+            'precio' => '1,50€'],
+
             ['descripcion' => 'Brioche', 
             'foto' => 'images/brioche.jpeg',
             'precio' => '1,50€'],
 
-            ['descripcion' => 'Croissant mantequilla',
+            
+            ['descripcion' => 'Croissant',
              'foto' => 'images/croissant.jpg',
              'precio' => '1,20€'],
 
             ['descripcion' => 'Pain au chocolat', 
             'foto' => 'images/painauchocolat.jpeg',
+            'precio' => '1,50€'],
+
+            ['descripcion' => 'Galette', 
+            'foto' => 'images/galette.jpg',
+            'precio' => '1,50€'],
+
+            ['descripcion' => 'Éclairs', 
+            'foto' => 'images/eclairs.jpeg',
+            'precio' => '1,50€'],
+
+            ['descripcion' => 'Paris-brest', 
+            'foto' => 'images/paris-brest.jpeg',
             'precio' => '1,50€']
 
             
@@ -44,6 +61,16 @@ class TiendaController extends AbstractController{
         #[Route('/')]
     public function homepage(){
         return $this->render('tienda/homepage.html.twig',[
+            'titulo' => $this->nombre,
+            'productos' => $this->productos
+
+        ]);
+    }
+
+
+    #[Route('/boulangerie')]
+    public function boulangerie(){
+        return $this->render('tienda/boulangerie.html.twig',[
             'titulo' => $this->nombre,
             'productos' => $this->productos
 
