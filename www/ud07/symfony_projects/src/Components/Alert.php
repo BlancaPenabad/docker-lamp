dd22b5415912110dd4e2ea040b7508327e3d7593
Blanca Penabad <a22blancapv@iessanclemente.com>
2024-05-14 23:49:38 +0200

    Retoques ejercicio UD6 Y COMIENZO UD8

---
+++ www/ud07/symfony_projects/src/Components/Alert.php
@@ -0,0 +1,13 @@
+<?php 
+namespace App\Components;
+
+use App\Service\PackageRepository;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('alert')]
+class Alert
+{
+
+
+}
+?>
