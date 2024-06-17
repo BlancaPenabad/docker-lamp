3583081d548f6d0423db72ed51b62bf2a38b42cf
Nombre de alumno <nombre@alumno>
2024-06-17 21:57:58 +0200

    Intento de hacer e ejercicio 2

--- www/ParcialT1Examen/e1_test.php
+++ www/ParcialT1Examen/e1_test.php
@@ -1,12 +1,13 @@
 <?php
 include('e1.php');
 $stringCualquiera = "Esto es un string cualquiera";
+
 echo "Dado el siguiente string: ".$stringCualquiera."</br>";
 echo "Función que cuenta vocales: ".contarVocales($stringCualquiera)."</br>";
 echo "Función que imprime vocales: ";
 $miArray = obtenerVocales($stringCualquiera);
 foreach ($miArray as $v) {
-    echo $v;
+    echo $v." ";
 }
 
 ?>
---
+++ www/ParcialT1Examen/e2.php
@@ -0,0 +1,67 @@
+<?php
+$universidad = array(
+        "Facultad de Ciencias" => array(
+            "Departamento de Física" => array(
+                "Profesores" => array(
+                    "Juan Pérez",
+                    "Ana García"
+                ),
+                "Cursos" => array(
+                    "Mecánica",
+                    "Electromagnetismo"
+                )
+        ),
+            "Departamento de Matemáticas" => array(
+                "Profesores" => array(
+                    "Carlos Sánchez",
+                    "María López"
+                ),
+                "Cursos" => array(
+                    "Álgebra",
+                    "Cálculo"
+                )
+        )
+    ),
+        "Facultad de Letras" => array(
+            "Departamento de Historia" => array(
+                "Profesores" => array(
+                    "José Ruiz",
+                    "Elena Fernández"
+                ),
+                "Cursos" => array(
+                    "Historia Antigua",
+                    "Historia Moderna"
+                )
+),
+            "Departamento de Literatura" => array(
+                "Profesores" => array(
+                    "Miguel Hernández",
+                    "Laura Martínez"
+                ),
+                "Cursos" => array(
+                    "Literatura Española",
+                    "Literatura Universal"
+                )
+        )
+    )
+);
+
+function imprimirProfesores($arrayUniversidad){
+
+    foreach($arrayUniversidad as $key=>$value){
+        if(is_array($value)){
+            //echo 'key:'.$key;
+           // echo"";
+            imprimirProfesores($value);
+        }else{
+            
+            echo $value."</br>";
+            echo"";
+        }
+    }
+}
+
+
+echo imprimirProfesores($universidad);
+
+?>
